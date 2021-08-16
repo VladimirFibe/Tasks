353b3ba4288bad3204db793ba15773da26b5e236
Vladimir Fibe <motiw@me.com>
2021-08-16 10:35:39 +0600

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Владимир on 16.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
